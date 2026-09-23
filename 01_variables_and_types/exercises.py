c12e47753b20538cfcea12d81fbf5afcd4e1cc14
tayande <ayandedavid2002@gmail.com>
2026-09-23 10:15:24 +0100

    Day 1: variables and basic data types

---
+++ 01_variables_and_types/exercises.py
@@ -0,0 +1,22 @@
+# Exercise 1
+my_name = "Terngu David Ayande"
+my_age = 24
+my_height = 6.5
+currently_learning_python = True
+print(type(my_name))
+print(type(my_age))
+print(type(my_height))
+print(type(currently_learning_python))
+print("===============")
+print()
+
+# Exercise 2
+a = 7
+b = "7"
+print(a == b)
+print(type(a), type(b))
+print("===============")
+print()
+
+
+
---
+++ 01_variables_and_types/notes.md
@@ -0,0 +1,1 @@
+A variable is just a name that points to a value in memory. The variable itself does not contain any value, it only points to one, and can be repointed to another at any time. 
